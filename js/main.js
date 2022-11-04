9427867e31deb2660a31e8f73ffe89ea9d3bb211
sayfutdiinov <sayfutdinovsaman.com>
2022-11-04 18:19:24 +0500

    added new style and fuction for search

--- js/main.js
+++ js/main.js
@@ -1,85 +1,165 @@
-// const elResultBox = document.querySelector(".result-box")
-// const elTemplate = document.querySelector(".template").content
-// let fragment = new DocumentFragment()
-// const APIKEY = "95800542e6fd46deb1f6c36a9a85e265"
 
-
-// const elSelectForm = document.querySelector(".form-select")
-
-
-// const elSearchForm = document.querySelector(".search-form")
-// const elSearchinput = document.querySelector(".input-search")
-
-
-// const elCategoryList=document.querySelector(".category-list");
-// const elPaginationList=document.querySelector(".pagination-list");
+//Dom result elemnts
+const elResultBox = document.querySelector(".result-box")
+const elTemplate = document.querySelector(".template").content
+let fragment = new DocumentFragment()
+const APIKEY = "1f08b0a9213045bea7a54e24bc34c982"
 
 
 
-// function makeList(array) {
-//     elResultBox.innerHTML = ""
-//     array.forEach(elm => {
-//         let cloneTemplate = elTemplate.cloneNode(true)
-//         cloneTemplate.querySelector(".link-to-site").href = elm.url
-//         cloneTemplate.querySelector(".item-title").textContent = elm.title
-//         cloneTemplate.querySelector(".sourse-name").textContent = elm.source.name
-//         cloneTemplate.querySelector(".news-img").src = elm.urlToImage
-//         fragment.appendChild(cloneTemplate)
-//     });
-//     elResultBox.appendChild(fragment)
-// }
+
+//serach elemnts
+const elSearchForm = document.querySelector(".search-form")
+const elSearchinput = document.querySelector(".input-search")
+
+//sort elements
+const elCategoryList=document.querySelector(".category-list");
+const elPaginationList=document.querySelector(".pagination-list");
+const elSelectForm=document.querySelector(".form-select");
+
+//modal elemtn
+const elModalTitle=document.querySelector(".modal-title");
+const elModalConent=document.querySelector(".modal-content-text");
+const elModalConentDescr=document.querySelector(".modal-content-description");
+                                                                                                                                                                                                                                                                                                                                                    
+const elModalUrl=document.querySelector(".btn-go-site");
 
 
-// elCategoryList.addEventListener("click",function (evt){
-//     evt.preventDefault()
-//     let newsCategory = evt.target.value
-//     categoryNews(newsCategory)
-// })
+// 
+let newsCategory
+let pegination 
+let selectValue
+let inputValue
+let data 
+
+//make li box news 
+function makeList(array) {
+    elResultBox.innerHTML = ""
+    array.forEach((elm) => {
+        let cloneTemplate = elTemplate.cloneNode(true)
+        cloneTemplate.querySelector(".item-title").textContent = elm.title
+        cloneTemplate.querySelector(".sourse-name").textContent = elm.source.name
+        cloneTemplate.querySelector(".news-img").src = elm.urlToImage
+        cloneTemplate.querySelector(".btn-info").dataset.idForModal=elm.source.name
+        cloneTemplate.querySelector(".news-time").textContent=`Data ${elm.publishedAt.slice(8,10)} ${elm.publishedAt.slice(5, 7)}-${elm.publishedAt.slice(0,4)} ${elm.publishedAt.slice(11,16)} `
 
 
-// async function newsTop() {
-//     try {
-//         const NEWS_TOP= `https://newsapi.org/v2/top-headlines?country=us&pageSize=9&apiKey=${APIKEY}`
-//         let send = await fetch(NEWS_TOP)
-//         let data = await send.json()
-//         makeList(data.articles)
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// async function categoryNews(btnvalue) {
-//     try {
-//         const NEWS_API = `https://newsapi.org/v2/everything?q=${btnvalue || "everything"}&apiKey=${APIKEY}`
-//         let send = await fetch(NEWS_API)
-//         let data = await send.json()
-//         makeList(data.articles)
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+        fragment.appendChild(cloneTemplate)
+    });
+    elResultBox.appendChild(fragment)
+}
 
 
-// async function getApi(inputvalue, selectvalue) {
-//     try {
-//         const NEWS_API = `https://newsapi.org/v2/everything?q=${inputvalue || "everything"}&sortBy=${selectvalue || "publishedAt"}&apiKey=${APIKEY}`
-//         let send = await fetch(NEWS_API)
-//         let data = await send.json()
-//         makeList(data.articles)
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// elSearchForm.addEventListener("submit", function (evt) {
-//     evt.preventDefault()
-//     let inputValue = elSearchinput.value
-//     let selectValue=elSelectForm.value
-
-//     getApi(inputValue, selectValue)
-// })
+// pegination style
+const elListBox=document.querySelectorAll(".pagination-list__item");
+const elListLink=document.querySelectorAll(".pagination-list__number")
 
 
-// newsTop()
+elListLink.forEach(function(links){
+
+    links.addEventListener("click",(evt)=>{
+        evt.preventDefault();
+        elListBox.forEach(function(box){
+            box.classList.remove("pageination-style")
+        })
+        links.parentElement.classList.add("pageination-style")
+    })
+
+})
 
 
+// pageination
+elPaginationList.addEventListener("click", function(ultarget) {
+    ultarget.preventDefault();
+    if (ultarget.target.dataset.page) {
+
+
+       
+        pegination = Number(ultarget.target.dataset.page)
+        getApi(inputValue, pegination)
+        categoryNews(newsCategory, pegination, selectValue)
+    }
+})
+
+
+// get categoriyes
+elCategoryList.addEventListener("click", function (evt) {
+    evt.preventDefault()
+    newsCategory = evt.target.value
+    selectValue=elSelectForm.value
+    categoryNews(newsCategory, pegination ,selectValue)
+})
+
+// default top news
+async function newsTop() {
+    try {
+        const NEWS_TOP= `https://newsapi.org/v2/top-headlines?country=us&p=tesla&pageSize=9&apiKey=${APIKEY}`
+        let send = await fetch(NEWS_TOP)
+        data = await send.json()
+        makeList(data.articles)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// filter categories
+async function categoryNews(btnvalue, newsPage,sortBy) {
+    try {
+        const NEWS_API = `https://newsapi.org/v2/everything?page=${newsPage || 2}&sortBy=${sortBy || "publishedAt"}&pageSize=9&q=${btnvalue || "everything"}&apiKey=${APIKEY}`
+        let send = await fetch(NEWS_API)
+        data = await send.json()
+        makeList(data.articles)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// filet input search
+async function getApi(inputvalue, page) {
+    try {
+        const NEWS_API = `https://newsapi.org/v2/everything?q=${inputvalue || "everything"}&page=${page || "1"}&pageSize=9&apiKey=${APIKEY}`
+        let send = await fetch(NEWS_API)
+        data = await send.json()
+        makeList(data.articles)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+
+
+function modalInfo(array){
+
+    let newObj=data.articles.find(elm=>{
+        return elm.source.name==array
+    })
+
+    console.log(newObj);
+
+    elModalTitle.textContent=newObj.title
+    elModalConent.textContent=newObj.content
+    elModalConentDescr.textContent=newObj.description
+    elModalUrl.href=newObj.url
+
+    
+}
+
+//form submit
+elSearchForm.addEventListener("submit", function(evt) {
+    evt.preventDefault()
+    inputValue = elSearchinput.value
+
+   getApi(inputValue, pegination)
+})
+
+
+// click more info
+elResultBox.addEventListener("click",function(evt){
+    if (evt.target.matches(".btn-info")) {
+        modalInfo(evt.target.dataset.idForModal)
+    }
+})
+
+// default top news
+newsTop()
